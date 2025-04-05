bec5e34196979f730849e02e4277240f54f35703
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2025-04-05 17:29:17 +0530

    pending shop registration

--- src/landingPages/RegisterShopComp.jsx
+++ src/landingPages/RegisterShopComp.jsx
@@ -3,6 +3,10 @@ import "../landingPages/RegisterShopComp.css"
 import listPng from "../uiuxassets/list.png"
 import filePng from "../uiuxassets/File.png"
 import evaluationPng from "../uiuxassets/evaluation.png"
+import IndiaFlag from "../uiuxassets/IndiaFlag.png"
+import dropdownimg from "../uiuxassets/arrow_drop_down.png"
+import mapBG from "../uiuxassets/Basemapimage.png"
+import { Checkbox } from 'antd'
 
 const RegisterShopComp = () => {
   return (
@@ -77,6 +81,75 @@ const RegisterShopComp = () => {
                     <input className='shopNameclassnameforbussinessregistration' type="text" name="shopName" id="" placeholder='   Bussiness / Store Name' />
                 </div>
             </div>
+
+
+            <div className="bussinessstorenamecompdivcard">
+                <div className="headingsubdivforcardregistrationbussiness">
+                    <p className="headingptagforbussinessregistration">
+                    Owner details
+                    </p>
+                    <p className="descriptionptagforbussinessregistration">
+                    We will use this details for all business communication and updates                    </p>
+                </div>
+                <div className="bussinessstorenameinputdiv newdiplayflexonlyforthisdiv">
+                    <input className='shopNameclassnameforbussinessregistration newwidthforfirstnameofowner' type="text" name="shopName" id="" placeholder='   Full name*' />
+                    <input className='shopNameclassnameforbussinessregistration newwidthforfirstnameofowner' type="text" name="shopName" id="" placeholder='   Email address*' />
+                </div>
+                <div className="bussinessstorenameinputdiv newdiplayflexonlyforthisdiv">
+                <div className='shopNameclassnameforbussinessregistration phonecuntryinputcode'>
+<img className='indianFlagImg' src={IndiaFlag} alt='IndiaFlag'/>
+<p className='pluscontrycodepara'>+91</p>
+<img src={dropdownimg} alt="" className="dropdownarrow" />
+                </div>
+                <input className='shopNameclassnameforbussinessregistration phoneinputnewWidth' type="text" name="shopName" id="" placeholder='   Phone number*' />
+                    </div>
+
+
+                    <div className="bussinessstorenameinputdiv">
+                    <p className="headingptagforbussinessregistration">
+                    Owner details
+                    </p>
+                    <p className="descriptionptagforbussinessregistration">
+                    We will use this details for all business communication and updates                    </p>
+                        </div>
+
+                        <div className="bussinessstorenameinputdiv">
+                            <div className="chockboxdivsameasowner shopNameclassnameforbussinessregistration newheightforcheckboxdivshop">
+                            <Checkbox style={{
+                                marginLeft:"20px"
+                            }}> Same as owner mobile number</Checkbox>
+                            </div>
+                        
+                        </div>
+
+            </div>
+
+            <div className="bussinessstorenamecompdivcard">
+                <div className="headingsubdivforcardregistrationbussiness">
+                    <p className="headingptagforbussinessregistration">
+                    Add your Business/Store’s location for order pickup
+                    </p>
+                
+                </div>
+
+                 <div className="bussinessstorenameinputdiv">
+                    <div className="shopNameclassnameforbussinessregistration newborderbottomsonly">
+                    <div className="shopNameclassnameforbussinessregistration">
+                            hgvhgv
+                        </div>
+                        <img src={mapBG} alt="" srcset="" className='gvhgvhgvhg656576'/>
+                       
+                        <p className="headingptagforbussinessregistration hghgvhgvhgv6576576">
+                        Sadashiv peth
+                    </p>
+                    <p className="descriptionptagforbussinessregistration hghgvhgvhgv6576576">
+                    Pune Station, pune                    </p>
+                    </div>
+                </div>
+              
+            </div>
+
+
         </div>
     </div>
   )
